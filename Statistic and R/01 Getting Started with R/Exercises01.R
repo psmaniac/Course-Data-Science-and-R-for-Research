dbe35155f203eb08dc7c73feaf2dffa13116dfc9
psmaniac <42663733+psmaniac@users.noreply.github.com>
2018-08-25 09:51:44 -0400

    Create Exercises01.R

---
+++ Statistic and R/01 Getting Started with R/Exercises01.R
@@ -0,0 +1,15 @@
+# If you have not done so already, download, install and load the swirl package.
+# install.packages("swirl")
+# library(swirl)
+# Go through the R Programming Basic Building Blocks tutorial and then use the skills you have just learned to answer the
+# following questions.
+
+# Exercise #1 -----------------------------------------------------------------------------------------------------------
+
+# What version of R are you using (hint: make sure you download the latest version and then type version)? Please note that
+# this question does not count toward your grade, but it is important to make sure that you are using the latest version of R. 
+#If the answer is not the MOST updated, please just let us know.
+
+# Exercise #2 ------------------------------------------------------------------------------------------------------------
+
+# Create a numeric vector containing the numbers 2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23. What is the average of these numbers?
